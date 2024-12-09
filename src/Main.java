35e20a844752ec3e676ddfb9d8885146e2fa4e7e
Ori Korngut <ori.korngut@gmail.com>
2024-12-09 22:16:17 +0200

    Started first function

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         int[] arr1 = new int[7];
         arr1 = new int[]{  4, -5, -3, 1, 2, 7, 9, 0};
         System.out.println(Arrays.toString(specialArr(arr1, 1)));
+        System.out.println(Arrays.toString(new int[]{first(arr1)}));
     }
 
     public static int[] specialArr (int[] arr, int med){
@@ -59,5 +60,58 @@ public class Main {
         }
 
         return output;
+    }
+
+    public static int first (int [] arr){
+        int counterN = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0){
+                counterN++;
+            }
+        }
+
+        int[] numbersFromN = new int[counterN];
+        counterN = 0;
+
+        for (int i = 0; i < numbersFromN.length; i++) {
+            if (arr[i] > 0){
+                numbersFromN[counterN] = arr[i];
+                counterN++;
+            }
+        }
+
+        for (int i = 0; i < numbersFromN.length; i++) {
+            if (i == 0){
+                continue;
+            }
+            else{
+                if (numbersFromN[i] - 1 == numbersFromN[i]){
+                    warp(numbersFromN, numbersFromN[i], numbersFromN[i-1]);
+                }
+            }
+        }
+
+        int answar = 1;
+
+        for (int i = 0; i < numbersFromN.length; i++) {
+            if (answar == numbersFromN[i]){
+                break;
+            }
+            else {
+                answar++;
+            }
+        }
+
+        return answar;
+
+    }
+
+    public static int[] warp(int[] array, int first, int second){
+        int helper = array[second];
+        array[second] = array[first];
+        array[first] = helper;
+
+        return array;
     }
 }
