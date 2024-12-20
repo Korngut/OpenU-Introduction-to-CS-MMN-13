1f0a335f32ebb866e5f9578df52e354ae9100787
Ori Korngut <ori.korngut@gmail.com>
2024-12-20 13:37:31 +0200

    Added quicksort

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
         int[] arr1 = new int[7];
         arr1 = new int[]{  4, -5, -3, 1, 2, 7, 9, 0};
+
+        System.out.println(Arrays.toString(quickSort(arr1, 0, arr1.length - 1)));
         System.out.println(Arrays.toString(specialArr(arr1, 1)));
         System.out.println(Arrays.toString(new int[]{first(arr1)}));
+        System.out.println(funRecursive(23));
     }
 
     public static int[] specialArr (int[] arr, int med){
@@ -65,7 +68,7 @@ public class Main {
     public static int first (int [] arr){
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] <= arr.length && arr[i] >= 1 && arr[i] - 1 != i) {
-                switchArrays(arr, arr[i] - 1, i);
+                swap(arr, arr[i] - 1, i);
             }
         }
 
@@ -80,10 +83,56 @@ public class Main {
 
     }
 
-    public static void switchArrays(int[] array, int first, int second){
+    public static void swap(int[] array, int first, int second){
         int helper = array[second];
         array[second] = array[first];
         array[first] = helper;
 
     }
+
+    public static int funRecursive(int num){
+        if (num == 1){
+            return 3;
+        }
+        if (num == 2){
+            return 7;
+        }
+        else {
+            return 2* funRecursive(num-1) + funRecursive(num-2);
+        }
+    }
+
+    private static int[] quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivot = partition(arr, left, right);
+
+            quickSort(arr, left, pivot - 1);
+            quickSort(arr, pivot + 1, right);
+        }
+        return arr;
+    }
+
+
+    private static int partition(int[] arr, int left, int right){
+        int pivot = arr[right];
+
+        return partitionRecursive(arr, left, right, pivot);
+    }
+
+    private static int partitionRecursive(int[] arr, int left, int right, int pivot){
+        int i = left - 1;
+
+        if (left >= right){
+            return i + 1;
+        }
+
+        if (arr[left] <= pivot) {
+            i++;
+            swap(arr, i, left);
+        }
+
+        return partitionRecursive(arr, left + 1, right, pivot);
+
+    }
+
 }
