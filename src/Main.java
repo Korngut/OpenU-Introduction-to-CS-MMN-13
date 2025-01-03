b1a240a0438331f0b12f60fa99ee4755ff73dfbb
Ori Korngut <ori.korngut@gmail.com>
2025-01-03 16:02:12 +0200

    יש כיוון לשאלה 3 ו4

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,17 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr1 = new int[7];
-        arr1 = new int[]{4, -5, -3, 1, 2, 7, 9, 0};
+        int[] arr1 = new int[]{1, 1, 1, 1, 10, 4, 3, 10};
+
+        int[][] matrix1 = {
+                {4, 5, 8, 2},
+                {3, 12, 7, 16},
+                {13, 1, 10, 14},
+                {15, 11, 9, 6}
+        };
+
+        System.out.println(longestNearlyPal(arr1));;
+        System.out.println(extreme(matrix1));;
 
         System.out.println(Arrays.toString(specialArr(arr1, 1)));
         System.out.println(Arrays.toString(new int[]{first(arr1)}));
@@ -46,6 +55,49 @@ public class Main {
 
         return arr.length;
 
+    }
+
+
+    public static int longestNearlyPal (int[] arr){
+        return longestNearlyPal(arr, 0, 0, false, 0);
+    }
+
+    private static int longestNearlyPal (int[] arr, int index1, int index2, boolean HasOmitted, int currectlength){
+        if (index1 > index2){
+            return currectlength;
+        }
+
+        if (arr[index1] == arr[index2] && !HasOmitted) {
+            return longestNearlyPal(arr, index1 + 1, index2 - 1, false, currectlength + 2);
+        }
+
+        if (arr[index1] != arr[index2] && !HasOmitted) {
+            return Math.max(
+                    longestNearlyPal(arr, index1 + 1, index2, true, currectlength),
+                    longestNearlyPal(arr, index1, index2 - 1, true, currectlength)
+            );
+        } else {
+            return currectlength;
+        }
+    }
+
+    public static int extreme(int [][] m){
+        return extreme(m,0,0,0);
+    }
+
+    public static int extreme(int [][] m, int currentX, int currentY, int currentLowVal){
+        int minValue = Math.min(currentLowVal, m[currentY][currentX]);
+
+        if (currentX == m[0].length - 1 && currentY == m.length - 1) {
+            return minValue;
+        }
+
+        int right = extreme(m, currentX + 1, currentY, minValue);
+        int left = extreme(m, currentX - 1, currentY, minValue);
+        int down = extreme(m, currentX, currentY + 1, minValue);
+        int up = extreme(m, currentX, currentY - 1, minValue);
+
+        return Math.min(Math.min(right, left), Math.min(down, up));
     }
 
     public static void swap(int[] array, int first, int second) {
