b102a10e4ea4a448e320ae704a972874aa92de7a
Ori Korngut <128257495+Korngut@users.noreply.github.com>
2025-01-15 22:30:08 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,135 +1,51 @@
-package com.company;
-import java.util.Arrays;
+/**
+ Solution - MMN 13
 
+ @version 2025a
+ @author Ori Korngut - 218666568
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println ("********** Question 1 **********\n");
-        int[] arr1 = {4, -5, -3, 1, 2, 7, 0};
-        int med1 = 1;
-        System.out.println("Checking method 'specialArr' on array "+toString(arr1)+" and med = " + med1);
-        int[] studentResult1;
-        studentResult1 = Main.specialArr(arr1, med1);
-        System.out.println("Result is: "+toString(studentResult1));
-        int result=checkIfSpecial(studentResult1);
-        if(result>=0)
-        {
-            System.out.println ("Error!!! The array is not a special array." );
-            System.out.println("The first problematic index is : "+result);
 
-        }
-        System.out.println();
-
-        int[] arr2 = {4, -5, -3, 1, 2, 7, 9, 0};
-        int med2 = 2;
-        System.out.println("Checking method 'specialArr' on array "+toString(arr2)+" and med = " + med2);
-        int[] studentResult2;
-        studentResult2 = Main.specialArr(arr2, med2);
-
-        System.out.println("Result is: "+ toString(studentResult2));
-        result=checkIfSpecial(studentResult2);
-        if(result>=0)
-        {
-            System.out.println ("Error!!! The array is not a special array." );
-            System.out.println("The first problematic index is : "+result);
-
-        }
-        System.out.println();
-        System.out.println("Note that these are not the only special arrays that can be created");
-        System.out.println();
-
-        System.out.println ("********** Question 2 **********\n");
-        int[] arr = new int[]{1, -3, 6, 2, 0, 15};
-        System.out.println("Checking method 'first' on array " + toString(arr));
-        int studentResult = Main.first(arr);
-        System.out.println("Result is: "+studentResult);
-        System.out.println();
-
-        arr = new int[]{1, 1, 1, 1};
-        System.out.println("Checking method 'first' on array " + toString(arr));
-        studentResult = Main.first(arr);
-        System.out.println("Result is: "+studentResult);
-        System.out.println();
-
-        arr = new int[]{1, 2, 3, 4};
-        System.out.println("Checking method 'first' on array " + toString(arr));
-        studentResult = Main.first(arr);
-        System.out.println("Result is: "+studentResult);
-        System.out.println();
-
-        arr = new int[]{5, -1, 3, 1, 0, -2, 2};
-        System.out.println("Checking method 'first' on array " + toString(arr));
-        studentResult = Main.first(arr);
-        System.out.println("Result is: "+studentResult);
-        System.out.println();
-
-        arr = new int[]{7, 8, 9, 11, 12, 14};
-        System.out.println("Checking method 'first' on array " + toString(arr));
-        studentResult = Main.first(arr);
-        System.out.println("Result is: "+studentResult);
-        System.out.println();
-
-        System.out.println ("********** Question 3 **********\n");
-        int[] array = {1, 1, 4, 10, 10, 4, 3, 10, 10};
-        System.out.println("Checking method 'longestNearlyPal' on array " + toString(array));
-        int studentResult3 = Main.longestNearlyPal(array);
-        System.out.println("Result is: "+studentResult3);
-        System.out.println();
-
-        System.out.println ("********** Question 4 **********\n");
-
-        int[][] a={{1,2},{3,4}};
-        System.out.println("Checking method 'extreme' on array ");
-        print(a);
-        int studentResult4 = Main.extreme(a);
-        System.out.println("Result is: "+studentResult4);
-        System.out.println();
-
-        int[][] b={{1,3},{4,2}};
-        System.out.println("Checking method 'extreme' on array ");
-        print(b);
-        studentResult4 = Main.extreme(b);
-        System.out.println("Result is: "+studentResult4);
-        System.out.println();
-
-        int[][] c={{4,5,8,2},{3,12,7,16},{13,1,10,14},{15,11,9,6}};
-        System.out.println("Checking method 'extreme' on array ");
-        print(c);
-        studentResult4 = Main.extreme(c);
-        System.out.println("Result is: "+studentResult4);
-        System.out.println();
-
-        int[][] d={{4,5,8,2},{3,12,16,7},{13,1,10,14},{15,11,9,6}};
-        System.out.println("Checking method 'extreme' on array ");
-        print(d);
-        studentResult4 = Main.extreme(d);
-        System.out.println("Result is: "+studentResult4);
-        System.out.println();
     }
 
-    public static int[] specialArr(int[] arr, int med) {
-        int[] output = arr.clone();
-        int counterUp = 1;
-        int counterDown = 0;
+    /**
+     Gets an array and turns it into a special array.
+     O(N) space complexity,
+     O(N) time complexity
+
+     @param arr The array that must be turned into a special array
+     @param med The median in the given array
+     @return An array with the same values, but in the form of a special array
+     */
+
+    public static int[] specialArr (int[] arr, int med){
+        int[] newArr = new int[arr.length];
+        int counterOdd = 1;
+        int counterEven = 0;
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > med) {
-                if (counterDown < arr.length) {
-                    output[counterDown] = arr[i];
-                    counterDown += 2;
-                }
-            } else {
-                if (counterUp < arr.length) {
-                    output[counterUp] = arr[i];
-                    counterUp += 2;
-                }
+            if (arr[i] >= med) {
+                newArr[counterEven] = arr[i];
+                counterEven += 2;
+            }
+            else{
+                newArr[counterOdd] = arr[i];
+                counterOdd += 2;
             }
         }
 
-        return output;
+        return newArr;
     }
 
+    /**
+     Gets an array and returns the first natural number that does not appear in the array
+     O(1) space complexity,
+     O(N) time complexity
 
-
+     @param arr The array in which to find the number
+     @return The first natural number not in the array
+     */
 
     public static int first(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
@@ -143,15 +59,22 @@ public class Main {
                 return i;
             }
         }
-
-
-        return arr.length;
+        return arr.length + 1;
 
     }
 
+    /**
+     Gets an array and returns the first natural number that does not appear in the array
+     O(1) space complexity,
+     O(N) time complexity
+
+     @param arr The array in which to find the number
+     @return The first natural number not in the array
+     */
     public static int longestNearlyPal(int[] arr) {
         return longestNearlyPal(arr, 0, arr.length - 1);
     }
+
     private static int longestNearlyPal(int[] arr, int start, int end) {
         if (start > end) {
             return 0;
@@ -163,6 +86,7 @@ public class Main {
         int removeEnd = longestNearlyPal(arr, start, end - 1);
         return Math.max(removeStart, removeEnd);
     }
+
     private static boolean isPalindrome(int[] arr, int start, int end) {
         if (start >= end) {
             return true;
@@ -217,40 +141,5 @@ public class Main {
         array[first] = helper;
 
     }
-
-    private static String toString(int[] arr)
-    {
-        String s = "{";
-        for(int i=0; i<arr.length-1; i++)
-            s+=arr[i]+", ";
-        return s+arr[arr.length-1]+"}";
-    }
-
-    private static void print(int[][] arr)
-    {
-        for(int i=0; i<arr.length; i++)
-        {
-            for(int j = 0; j < arr[i].length-1; j++)
-                System.out.print(arr[i][j]+"\t");
-            System.out.println(arr[i][arr[i].length-1]);
-        }
-    }
-
-    private static int checkIfSpecial(int []arr){
-        int i;
-
-        for(i=0;i<arr.length-1;i++)
-            if(i%2==0)//even place
-            {
-                if(arr[i]<arr[i+1])//incorrect
-                    return i;
-            }
-            else//odd place
-                if(arr[i]>arr[i+1])//incorrect
-                    return i;
-
-        return -1;//special array
-    }
-
 
 }
